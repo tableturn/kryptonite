3b8dbd71595705df9b1541d4f4cd468c61e35d74
Pierre Martin <hickscorp@gmail.com>
2018-05-17 01:14:05 +0100

    Adds bang functions allowing easy piping of keys and random related functions.

--- lib/kryptonite/random.ex
+++ lib/kryptonite/random.ex
@@ -8,18 +8,32 @@ defmodule Kryptonite.Random do
 
   ## Examples
 
-      iex> {:ok, data} = bytes(16)
+      iex> data = bytes!(16)
       iex> {byte_size(data), bit_size(data)}
       {16, 128}
-      iex> {:ok, data} = bytes(32)
+      iex> data = bytes!(32)
       iex> {byte_size(data), bit_size(data)}
       {32, 256}
+  """
+  @spec bytes!(integer) :: binary
+  def bytes!(length), do: :crypto.strong_rand_bytes(length)
+
+  @doc """
+  Returns a string at random where the length is equal to `length`.
+  Essentially the same as `bytes!/1` but returns a tuple on error
+  instead of raising.
+
+  ## Examples
+
+      iex> data = bytes!(16)
+      iex> {byte_size(data), bit_size(data)}
+      {16, 128}
       iex> bytes(-1)
       {:error, :badarg}
   """
   @spec bytes(integer) :: {:ok, binary} | {:error, any}
   def bytes(length) do
-    {:ok, :crypto.strong_rand_bytes(length)}
+    {:ok, bytes!(length)}
   catch
     _, e -> {:error, e}
   end
