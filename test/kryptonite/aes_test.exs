740cd35ccf9b2bb0d73277824c54265b6ed91fa1
Pierre Martin <hickscorp@gmail.com>
2018-05-17 01:17:13 +0100

    Groups doctest runner, adds integration tests.

--- test/kryptonite/aes_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule Kryptonite.AESTest do
-  use ExUnit.Case, async: true
-  doctest Kryptonite.AES, import: true
-end
--- test/kryptonite/random_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule Kryptonite.RandomTest do
-  use ExUnit.Case, async: true
-  doctest Kryptonite.Random, import: true
-end
