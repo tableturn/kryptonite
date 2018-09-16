cd4e312a1ca11655fa5948053f54b796e37e8430
Pierre Martin <hickscorp@gmail.com>
2018-09-16 13:09:09 +0100

    Bumps version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kryptonite.MixProject do
   def project do
     [
       app: :kryptonite,
-      version: "0.1.10",
+      version: "0.1.11",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
